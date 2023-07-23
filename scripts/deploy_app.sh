d807fd61e87fd72285fd5856fe625b0cccaa1cf7
seyikayode <oluwaseyikayode25@gmail.com>
2023-07-23 02:18:35 +0100

    try s3

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -3,6 +3,5 @@
 helm package $HELM_CHART_NAME
 export HELM_EXPERIMENTAL_OCI=1
 helm push $HELM_CHART_NAME-0.1.0.tgz oci://$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/
-helm upgrade --install $HELM_CHART_NAME oci://$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$HELM_CHART_NAME
 echo "oci://$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$HELM_CHART_NAME"
 helm upgrade --install $HELM_CHART_NAME oci://$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$HELM_CHART_NAME
