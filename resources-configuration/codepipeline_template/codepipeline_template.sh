64d994844e5d925504f8a9d987961adf4e80d33e
seyikayode <oluwaseyikayode25@gmail.com>
2023-07-23 12:45:28 +0100

    github id

--- resources-configuration/codepipeline_template/codepipeline_template.sh
+++ resources-configuration/codepipeline_template/codepipeline_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export AWS_PROFILE=kayode
-aws cloudformation create-stack --stack-name create-codepipeline-resource --template-body file://codepipeline_template.yaml --parameter file://codepipeline_template.json --region=us-east-1
+aws cloudformation create-stack --stack-name create-codepipeline-resource --template-body file://codepipeline_template.yaml --parameter file://codepipeline_template.json --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
 
 # chmod +x codepipeline_template.sh
 # ./codepipeline_template.sh create-codepipeline-resource codepipeline_template.yaml codepipeline_template.json
