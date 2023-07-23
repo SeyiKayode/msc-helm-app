b1a36b5ff67cbfea7100addce892dd6effcf6529
seyikayode <oluwaseyikayode25@gmail.com>
2023-07-23 18:26:10 +0100

    yaml file

--- resources-configuration/codepipeline_template/codepipeline_template.sh
+++ resources-configuration/codepipeline_template/codepipeline_template.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+# aws configure --profile kayode
+# then export the profile below
 export AWS_PROFILE=kayode
 aws cloudformation create-stack --stack-name create-codepipeline-resource --template-body file://codepipeline_template.yaml --parameter file://codepipeline_template.json --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
-
-# chmod +x codepipeline_template.sh
-# ./codepipeline_template.sh create-codepipeline-resource codepipeline_template.yaml codepipeline_template.json
-# ./codepipeline_template.sh
